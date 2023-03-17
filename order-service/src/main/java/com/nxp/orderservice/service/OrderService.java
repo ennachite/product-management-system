e4fb3ae25c6f98789879df6ad0f0b709ee46a698
Oussama Ennachite <oussama.ennachite@nxp.com>
2023-03-17 11:41:57 +0100

    add discover server and configure it with services / add api-gateway

--- order-service/src/main/java/com/nxp/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/nxp/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -39,8 +39,9 @@ public class OrderService {
                 .toList();
 
 //        Call inventory service, and place order if product is in stock
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
-                .uri("http://localhost:8091/api/inventory",
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build()
+                .get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
